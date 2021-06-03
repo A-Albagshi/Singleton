1fb174530760b2e826754af6ed491332d9e4e6e0
Abdullah Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-06-03 09:28:59 +0300

    feat:Singleton Class

---
+++ SingletonDesign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SingletonDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singletone singletone1 = Singletone.getInstance();
+            Singletone singletone2 = Singletone.getInstance();
+
+            Console.WriteLine(singletone1 == singletone2); // Only One Instance of Singleton
+        }
+    }
+
+    class Singletone
+    {
+        private static Singletone instance;
+
+        private Singletone() { }
+
+        public static Singletone getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singletone();
+            }
+
+            return instance;
+        }
+    }
+}
